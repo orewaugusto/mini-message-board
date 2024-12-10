dd6b0599635cc753d43460155a8678b97a8a632c
Augusto <danielaugustolopes44@gmail.com>
2024-12-10 01:24:16 -0300

    Display messages in the homepage

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.use("/", indexRouter);
-app.use("/new", newRouter);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+const messages = [
+  {
+    text: "How d'ya",
+    user: "Arthur",
+    added: new Date()
+  },
+  {
+    text: "Hey mister",
+    user: "John",
+    added: new Date()
+  }
+];
+
+app.get("/", (req, res) => {
+  res.render("index", { messages: messages });
+})
 
 const PORT = 3000;
 app.listen(PORT, () =>{
